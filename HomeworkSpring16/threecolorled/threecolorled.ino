2321f200a4879121fa8c4a35324a1c8166b71cc6
Your Name <you@example.com>
2016-03-08 14:57:25 -0500

    arduino led games

---
+++ HomeworkSpring16/threecolorled/threecolorled.ino
@@ -0,0 +1,41 @@
+int redPin = 8;  //blue
+int greenPin = 9;
+int bluePin = 10;
+
+int redValue = 0;
+
+int button1 = 2;
+int button2 = 3;
+int button3 = 4;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  pinMode(button1, INPUT_PULLUP);
+  pinMode(button2, INPUT_PULLUP);
+  pinMode(button3, INPUT_PULLUP);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (digitalRead(button1) == HIGH) {
+    redValue = 0;
+  } else {
+    redValue = 255;
+  }
+  analogWrite(redPin, redValue);
+  if (digitalRead(button2) == HIGH) {
+    analogWrite(greenPin, 0);
+  } else {
+    analogWrite(greenPin, 255);
+  }
+  if (digitalRead(button3) == HIGH) {
+    analogWrite(bluePin, 0);
+  } else {
+    analogWrite(bluePin, 255);
+  }
+  delay(10);
+}
